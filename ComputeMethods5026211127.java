88839857cbbbe9b8a3cc512e0d28869912b576ab
aisadira <100084532+aisadira@users.noreply.github.com>
2022-02-27 20:31:52 +0700

    Add files via upload

---
+++ ComputeMethods5026211127.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class ComputeMethods5026211127	{
+
+	public double fToC(double degreesF)	{
+
+		return 5/9.0*(degreesF - 32);
+	}
+	public double hypotenuse ( int a, int b)	{
+
+		return Math.sqrt(a * a + b * b);
+	}
+	public int roll ()	{
+		Random rndm = new Random();
+		int dice1 = rndm.nextInt(6)+1;
+		int dice2 = rndm.nextInt(6)+1;
+		return dice1 + dice2;
+	}
+
+}
